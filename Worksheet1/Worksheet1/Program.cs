434627f5c5f9ca1f99c7da9f445a4c78a0d1ca4c
Elaine Farrugia <>
2024-10-10 13:24:55 +0200

    Worksheet 1

---
+++ Worksheet1/Worksheet1/Program.cs
@@ -0,0 +1,13 @@
+﻿string sequence = "w5Gh23pk";
+
+Console.WriteLine("Remember this: "+sequence);
+Thread.Sleep(5000);
+Console.Clear();
+
+Console.WriteLine("Enter the sequence:");
+string input = Console.ReadLine();
+
+if (input == sequence)
+    Console.WriteLine("Congrats, you guessed!");
+else
+    Console.WriteLine("You did not guess");
