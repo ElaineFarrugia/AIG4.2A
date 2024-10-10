f7a82eea669b84857b34ff224436d578a5e129f0
Elaine Farrugia <>
2024-10-10 13:25:05 +0200

    Demo2

---
+++ Demo2/Demo2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter your theory mark.");
+string theory = Console.ReadLine();
+
+Console.WriteLine("Enter your practical mark.");
+string practical = Console.ReadLine();
+
+int theoryInt = Convert.ToInt32(theory);
+int practicalInt = Convert.ToInt32(practical);
+
+if ((theoryInt >= 50) && (practicalInt >= 50))
+    Console.WriteLine("You passed!");
+else
+    Console.WriteLine("You did not pass");
+
